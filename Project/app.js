48c57f6773db8b3462efebb61cca8ec9c108e6c6
Jeffrey Luo <32887802+jeffluo1@users.noreply.github.com>
2018-11-20 22:06:57 -0700

    node/express addendums

--- Project/app.js
+++ Project/app.js
@@ -1,21 +1,69 @@
-const http = require('http');
-const fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressValidator = require('express-validator');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var app = express();
+/*
+var logger = function(req, res, next) {
+	console.log('Logging...');
+	next();
+}
 
-fs.readFile('index.html', (err, html) => {
-	if (err) {
-		throw err;
-	} 
-	const server = http.createServer((req, res) => {
-		res.statusCode = 200;
-		res.setHeader('Content-type', 'text/html');
-		res.write(html);
-		res.end('Hello World');
-	});
+app.use(logger);
+*/
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-	server.listen(port, hostname, () => {
-		console.log('Server started on port ' + port);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({entended: false}));
+
+//Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+var users = [
+	{
+		id: 1,
+		username: 'admin',
+		password: 'admin',
+		email: 'admin@gmail.com'
+	}, 
+	{
+		id: 2,
+		username: 'admin2',
+		password: 'admin2',
+		email: 'admin2@gmail.com'
+	}, 
+	{
+		id: 3,
+		username: 'admin3',
+		password: 'admin3',
+		email: 'admin3@gmail.com'
+	}
+]
+
+app.get('/', function(req, res){
+	res.render('index', {
+		title: 'Customers',
+		users: users
 	});
 });
+
+app.post('/users/add', function(req, res){
+	var newUser = {
+		username: req.body.username,
+		password: req.body.password,
+		email: req.body.email
+	}
+	console.log(newUser);
+	
+});
+
+app.listen(3000, function(){
+	console.log()
+})
+
+
+
+
