a9ae1ab7dc88186e09f76678f3288ee5f6d76c3c
Jeffrey Luo <32887802+jeffluo1@users.noreply.github.com>
2018-11-21 03:04:48 -0700

    mongoDB attempt, switching over some functionality with create post

--- Project/app.js
+++ Project/app.js
@@ -1,69 +1,42 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var expressValidator = require('express-validator');
-
+var express = require("express");
 var app = express();
-/*
-var logger = function(req, res, next) {
-	console.log('Logging...');
-	next();
-}
-
-app.use(logger);
-*/
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-
+var port = 3000;
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({entended: false}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//Set Static Path
-app.use(express.static(path.join(__dirname, 'public')));
+//HOMEPAGE, DEFAULT PATH
+app.use(express.static(__dirname));
 
-var users = [
-	{
-		id: 1,
-		username: 'admin',
-		password: 'admin',
-		email: 'admin@gmail.com'
-	}, 
-	{
-		id: 2,
-		username: 'admin2',
-		password: 'admin2',
-		email: 'admin2@gmail.com'
-	}, 
-	{
-		id: 3,
-		username: 'admin3',
-		password: 'admin3',
-		email: 'admin3@gmail.com'
-	}
-]
+//SETUP DATABASE
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-demo");
+var postSchema = new mongoose.Schema({
+    postTitle: String,
+    postContent: String
+});
+var newPost = mongoose.model("Post", postSchema);
 
-app.get('/', function(req, res){
-	res.render('index', {
-		title: 'Customers',
-		users: users
-	});
+//Commented out default path
+/*
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/create-post.html");
+});
+*/
+
+//POST SAVES TO DATABASE
+app.post("/addPost", (req, res) => {
+    var myData = new newPost(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Post Created, Redirecting Shortly");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
 });
 
-app.post('/users/add', function(req, res){
-	var newUser = {
-		username: req.body.username,
-		password: req.body.password,
-		email: req.body.email
-	}
-	console.log(newUser);
-	
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
 });
-
-app.listen(3000, function(){
-	console.log()
-})
-
-
-
-
