3290a509756395c1924f8cdb42579b78a4e6d361
Anthony Trang <halfsteel2@gmail.com>
2018-11-21 15:43:34 -0700

    will describe later, register and login work

--- Project/app.js
+++ Project/app.js
@@ -1,7 +1,4 @@
 var express = require("express");
-var router = express.Router();
-var mongo = require('mongodb');
-var assert = require('assert');
 var app = express();
 var port = 3000;
 var bodyParser = require('body-parser');
@@ -14,15 +11,14 @@ app.use(express.static(__dirname));
 //SETUP DATABASE
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/CPI310");
-
-//CREATING POST FIELDS
+mongoose.connect("mongodb://localhost:27017/node-demo");
 var postSchema = new mongoose.Schema({
     postTitle: String,
     postContent: String
 });
+var newPost = mongoose.model("Post", postSchema);
 
-var post = mongoose.model("Post", postSchema);
+
 
 //Commented out default path
 /*
@@ -31,61 +27,164 @@ app.get("/", (req, res) => {
 });
 */
 
-app.get('/template', function(req, res){
-	post.find({}, function(err, docs){
-		if(err) res.json(err);
-		else	res.render('index', {posts: docs});
-	});
-	
-	/*
-	mongo.connect(url, function(err, db) {
-		var postArray = [];
-		assert.equal(null,error);
-		var cursor = db.collection('post-data').find();
-		cursor.forEach(function(doc, err){
-			assert.equal(null, err);
-			postArray.push(doc);
-		}, function(){
-			db.close();
-			res.render('template', {posts: postArray})
-		});
-	});
-	*/
-});
-
 //POST SAVES TO DATABASE
 app.post("/addPost", (req, res) => {
-	new post({
-		postTitle: req.body.postTitle,
-		postContent: req.body.postContent
-	}).save(function(err,doc){
-		if(err) res.json(err);
-		else	res.redirect('template');
-	})
-
-
-	res.redirect('template.html');
-
-	mongo.connect(url, function(err, db) {
-		assert.equal(null, err);
-		db.collection('post-data').insertOne(post, function(err, result) {
-			assert.equal(null, err);
-			console.log('Post inserted');
-			db.close();
-		});
-	});
-	/*
-    const post = new Post(req.body);
-	post.save()
+    var myData = new newPost(req.body);
+    myData.save()
         .then(item => {
-			res.send("Post Created, Redirecting Shortly");
-			console.log(post);
+            res.send("Post Created, Redirecting Shortly");
         })
         .catch(err => {
             res.status(400).send("Unable to save to database");
-	});
-	*/
+        });
 });
+
+const sqlite = require('sqlite');
+const bcrypt = require('bcrypt');
+const uuidv4 = require('uuid/v4');
+const cookieParser = require('cookie-parser')
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'twig');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.disable('view cache');
+const saltRounds = 10;
+const dbPromise = sqlite.open('./data.db');
+
+dbPromise.then(async (db) => {
+    await db.run('CREATE TABLE IF NOT EXISTS messages ( id INTEGER PRIMARY KEY, author STRING, message STRING );');
+    await db.run('CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY, email STRING, passwordHash STRING );');
+    await db.run('CREATE TABLE IF NOT EXISTS sessions ( id INTEGER PRIMARY KEY, userid INTEGER, sessionToken STRING );');
+});
+
+//asdf
+const authorize = async (req, res, next) => {
+    // const { sessionToken } = req.cookies;
+    const db = await dbPromise;
+    const sessionToken = req.cookies.sessionToken;
+    if(!sessionToken) {
+        next();
+        return;
+    };
+    const user = await db.get('SELECT users.email, users.id as id FROM sessions LEFT JOIN users ON sessions.userid = users.id WHERE sessionToken=?', sessionToken);
+    if(!user) {
+        next();
+        return;
+    };
+    console.log('logged in', user.email);
+    req.user = user;
+    next();
+    return;
+};
+
+const requireAuth = (req, res, next) => {
+    if (!req.user) {
+        res.status(401).send('please log in');
+        return;
+    }
+    next();
+};
+
+app.use(authorize);
+
+app.get('/', async (req, res) => {
+    const db = await dbPromise;
+    const messages = await db.all('SELECT * FROM messages;');
+    const user = req.user;
+    res.render('index',{ messages, user });
+});
+
+app.get('/login', async (req, res) => {
+    if (req.user) {
+        res.redirect('/');
+        return;
+    }
+});
+
+app.get('/register', async (req, res) => {
+    if (req.user) {
+        res.redirect('/');
+        return;
+    }
+    res.render('register');
+});
+
+app.post('/message', requireAuth, async (req, res) => {
+    const db = await dbPromise;
+    await db.run('INSERT INTO messages (author, message) VALUES (?, ?)', req.user.email.split('@')[0], req.body.message);
+    res.redirect('/');
+});
+
+app.post('/register', async (req, res) => {
+    const db = await dbPromise;
+    const user = await db.get('SELECT * FROM users WHERE email=?', req.body.email);
+    if (user) {
+        res.status(400).render('register', { registerError: 'account already exists' });
+        return;
+    }
+    const passwordHash = await bcrypt.hash(req.body.password, saltRounds);
+    await db.run(
+        'INSERT INTO users (email, passwordHash)  VALUES (?, ?);',
+        req.body.email,
+        passwordHash
+    );
+    const newUser = db.get('SELECT id, email FROM users WHERE email=?', req.body.email);
+    const sessionToken = uuidv4();
+    await db.run(
+        'INSERT INTO sessions (userid, sessionToken) VALUES (?, ?);',
+        newUser.id,
+        sessionToken
+    );
+    res.cookie('sessionToken', sessionToken);
+    res.redirect('/');
+});
+
+app.post('/login', async (req, res) => {
+    const db = await dbPromise;
+    const user = await db.get('SELECT * FROM users WHERE email=?', req.body.email);
+    if (!user) {
+        { loginError: 'email or password is incorrect' };
+        return;
+    }
+    const passwordMatches = await bcrypt.compare(req.body.password, user.passwordHash);
+    if (passwordMatches) {
+        const sessionToken = uuidv4();
+        await db.run('INSERT INTO sessions (userid, sessionToken) VALUES (?, ?);', user.id, sessionToken);
+        res.cookie('sessionToken', sessionToken);
+        res.redirect('/');
+    } else {
+        { loginError: 'email or password is incorrect' };
+    }
+})
+
+app.get('/logout', async (req, res) => {
+    const db = await dbPromise;
+    res.cookie('sessionToken', '', { maxAge: 0 });
+    await db.run('DELETE FROM sessions WHERE sessionToken=?', req.cookies.sessionToken);
+    res.redirect('/');
+})
+
+app.get('/databasedump', async (req, res) => {
+    const db = await dbPromise;
+    const tables = await db.all('SELECT name FROM sqlite_master WHERE type="table"');
+    const users = await db.all('SELECT * FROM users');
+    const messages = await db.all('SELECT * FROM messages');
+    const sessions = await db.all('SELECT * FROM sessions');
+    res.json({
+        tables,
+        users,
+        messages,
+        sessions
+    })
+})
+
+app.use((req, res) => {
+    res.status(404).send('file not found');
+})
+
 
 app.listen(port, () => {
     console.log("Server listening on port " + port);
